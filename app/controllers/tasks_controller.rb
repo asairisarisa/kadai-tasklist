c5ae77985ec66367d6309fe637071c4d8ceb3973
risaasai <asairisa2552nc@gmail.com>
2017-04-17 04:36:27 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -55,4 +55,8 @@ class TasksController < ApplicationController
   def set_task
     @task = Task.find(params[:id])
   end
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
 end
