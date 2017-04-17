19e38d50dbec1526746f84da40115d58e4d02459
risaasai <asairisa2552nc@gmail.com>
2017-04-17 04:53:56 +0000

    CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only:[:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    @tasks = Task.order(created_at: :desc).page(params[:page]).per(3)
   end
 
   def show
@@ -20,7 +20,7 @@ class TasksController < ApplicationController
       redirect_to @task
     else
       flash.now[:danger] = "Taskが投稿されませんでした"
-      render :now
+      render :new
     end
   end
 
